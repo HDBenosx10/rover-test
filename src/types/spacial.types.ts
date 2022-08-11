bade38f3832aabe5d328a59a78a7baeb02e9184a
hdbenosx10 <benosx03@gmail.com>
2022-08-11 18:50:02 -0300

    lint: spaces in types

--- src/types/spacial.types.ts
+++ src/types/spacial.types.ts
@@ -1,4 +1,4 @@
 export type Cardinals = 'N' | 'E' | 'S' | 'W';
 export type Directions = 'L' | 'R';
 export type RoverCordinates = { x: number, y: number, facing: Cardinals };
-export type RoverWithInstructions = {cordinates: RoverCordinates, instructions: string[]};
+export type RoverWithInstructions = { cordinates: RoverCordinates, instructions: string[] };
